3eb4b23098ba206c886e4583f3d6d81c4cf5b0aa
ashutosh.dwivedi <ashutosh.dwivedi@servicenow.com>
2023-07-17 22:08:05 +0530

    completed chat screen and recent chats

---
+++ lib/screens/chat_screen.dart
@@ -0,0 +1,162 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_chat_ui_starter/models/message_model.dart';
+import 'package:flutter_chat_ui_starter/models/user_model.dart';
+
+class ChatScreen extends StatefulWidget {
+  //const ChatScreen({Key? key}) : super(key: key);
+  final User user;
+  ChatScreen({required this.user});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  _buildMessage(Message message, bool isMe) {
+    final msg = Container(
+      width: MediaQuery.of(context).size.width * 0.75,
+      margin: isMe
+          ? EdgeInsets.only(top: 8.0, bottom: 8.0, left: 80.0)
+          : EdgeInsets.only(top: 8.0, bottom: 8.0),
+      padding: EdgeInsets.symmetric(horizontal: 25.0, vertical: 15.0),
+      decoration: BoxDecoration(
+          color: isMe
+              ? Theme.of(context).colorScheme.secondary
+              : Color(0xFFFFEFEE),
+          borderRadius: isMe
+              ? BorderRadius.only(
+                  topLeft: Radius.circular(15.0),
+                  bottomLeft: Radius.circular(15.0))
+              : BorderRadius.only(
+                  topRight: Radius.circular(15.0),
+                  bottomRight: Radius.circular(15.0))),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            message.time,
+            style: TextStyle(
+              color: Colors.blueGrey,
+              fontSize: 16.0,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          SizedBox(
+            height: 8.0,
+          ),
+          Text(
+            message.text,
+            style: TextStyle(
+              color: Colors.blueGrey,
+              fontSize: 16.0,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+        ],
+      ),
+    );
+    if (isMe) return msg;
+
+    return Row(
+      children: [
+        msg,
+        IconButton(
+          icon: message.isLiked
+              ? Icon(Icons.favorite)
+              : Icon(Icons.favorite_border),
+          iconSize: 30.0,
+          color: message.isLiked
+              ? Theme.of(context).colorScheme.primary
+              : Colors.blueGrey,
+          onPressed: () {},
+        )
+      ],
+    );
+  }
+
+  _buildMessageComposer() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 8.0),
+      height: 70.0,
+      color: Colors.white,
+      child: Row(children: [
+        IconButton(
+          icon: Icon(Icons.photo),
+          iconSize: 26.0,
+          color: Theme.of(context).colorScheme.primary,
+          onPressed: () {},
+        ),
+        Expanded(
+            child: 
+            TextField(
+              textCapitalization: TextCapitalization.sentences,
+              onChanged: (value) {
+                
+              },
+              decoration: InputDecoration.collapsed(hintText: 'Send a message...'),
+        )),
+        IconButton(
+          icon: Icon(Icons.send),
+          iconSize: 26.0,
+          color: Theme.of(context).colorScheme.primary,
+          onPressed: () {},
+        )
+      ]),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.primary,
+      appBar: AppBar(
+        title: Text(
+          widget.user.name,
+          style: TextStyle(fontSize: 28.0, fontWeight: FontWeight.bold),
+        ),
+        elevation: 0.0,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.more_horiz),
+            iconSize: 30.0,
+            color: Colors.white,
+            onPressed: () {},
+          )
+        ],
+      ),
+      body: GestureDetector(
+        onTap: () => FocusScope.of(context).unfocus(),
+        child: Column(
+          children: [
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(30.0),
+                        topRight: Radius.circular(30.0))),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30.0),
+                      topRight: Radius.circular(30.0)),
+                  child: ListView.builder(
+                    reverse: true,
+                    padding: EdgeInsets.only(top: 15.0),
+                    itemCount: messages.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      final Message message = messages[index];
+                      final bool isMe = message.sender.id == currentUser.id;
+                      return _buildMessage(message, isMe);
+                      //return Text(message.text);
+                    },
+                  ),
+                ),
+              ),
+            ),
+            _buildMessageComposer()
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_chat_ui_starter/widgets/category_selector.dart';
 import 'package:flutter_chat_ui_starter/widgets/favourite_contacts.dart';
+import 'package:flutter_chat_ui_starter/widgets/recent_chats.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -13,45 +14,41 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.primary,
       appBar: AppBar(
-          leading: IconButton(
-            icon: Icon(Icons.menu),
-            iconSize: 30.0,
-            color: Colors.white,
-            onPressed: () {},
-          ),
-          title: Text('Chats',
-              style: TextStyle(fontSize: 28.0, fontWeight: FontWeight.bold)),
-          elevation: 0.0,    
-          actions: [IconButton(
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          iconSize: 30.0,
+          color: Colors.white,
+          onPressed: () {},
+        ),
+        title: Text('Chats',
+            style: TextStyle(fontSize: 28.0, fontWeight: FontWeight.bold)),
+        elevation: 0.0,
+        actions: [
+          IconButton(
             icon: Icon(Icons.search),
             iconSize: 30.0,
             color: Colors.white,
             onPressed: () {},
           )
-          ],
+        ],
+      ),
+      body: Column(children: [
+        CategorySelector(),
+        Expanded(
+          child: Container(
+            decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.secondary,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(30.0),
+                  topRight: Radius.circular(30.0),
+                )),
+            child: Column(children: [
+              FavouriteContacts(),
+              RecentChats()
+            ]),
           ),
-          body: Column(children: [
-            CategorySelector(),
-            Expanded(
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Theme.of(context).colorScheme.secondary,
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(30.0),
-                    topRight: Radius.circular(30.0),
-                  )
-            
-                  ),
-                  child: Column(
-                    children: [
-                   FavouriteContacts()
-                  ]),
-                ),
-            )
-            ]
-          )
-          ,
-
+        )
+      ]),
     );
   }
 }
